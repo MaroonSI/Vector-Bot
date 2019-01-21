171ebe1a9bd2ea0bf7e5f0bdb18df01f7103621e
MaroonSI <46507330+MaroonSI@users.noreply.github.com>
2019-01-21 13:51:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,14 @@
 const Discord = require('discord.js');
-const auth = require('./auth.json');
-const config = require('./config.json');
+const client = new Discord.Client();
 
-const bot = new Discord.Client();
-
-bot.on('ready', function() {
-    console.log("Bot is ready!");
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-bot.on('message', function(message) {
-    
-    // ignore messages sent via direct message
-    if (message.channel.type === "dm") {
-        return;
-    }
-    
-    // ignore messages sent from another bot
-    if (message.author.bot) {
-        return;
-    }
-    
-    // check if the message is a command (starting with the command prefix '$')
-    if (message.content.startsWith(config.prefix)) {
-        
-        // create an array that contains command name and arguments
-        // ".slice(config.prefix.length)" removes the prefix
-        // ".split(' ')" splits the message at spaces and puts the words
-        // into separate array entries
-        let content = message.content.slice(config.prefix.length).split(' ');
-        
-        let num_arguments = content.length;
-        
-        // check for the special case that a simple "$" was written with no command
-        if (num_arguments == 0) {
-            return;
-        }
-        
-        // now we can identify the command
-        let command = content[0];
-        
-        if (command === "hey") {
-            // check if there is a second argument that says "me"
-            if ((num_arguments > 1) && (content[1] === "me")) {
-                // ".reply" prepends an "@User," to the message,
-                // where User is the message's author
-                message.reply("Hey there crocodile!");
-                return;
-            }
-            
-            // this just sends a normal message in the chat
-            message.channel.send("Hey there crocodile!");
-            return;
-        }
-        
-        // add more commands here!
+client.on('message', () => {
+    if (message.content === 'yo') {
+        message.reply('Sup Crocodile!');
     }
 });
 
-bot.login(auth.token);
+client.login(process.env.BOT_TOKEN);
